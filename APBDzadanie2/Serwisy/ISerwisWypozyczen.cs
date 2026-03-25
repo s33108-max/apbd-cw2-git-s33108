dae32de204d14cd7edf09df6de12e1a3a2d10bc5
Klaudia Rutkowska <s33108@pjwstk.edu.pl>
2026-03-25 23:07:00 +0100

    serwiswypozyczen

--- APBDzadanie2/Serwisy/ISerwisWypozyczen.cs
+++ APBDzadanie2/Serwisy/ISerwisWypozyczen.cs
@@ -1,6 +1,17 @@
-﻿namespace APBDzadanie2.Serwisy;
+﻿using APBDzadanie2.Wyjatki;
+using APBDzadanie2.Modele;
+using APBDzadanie2.Enums;
+using System.Linq;
+
+namespace APBDzadanie2.Serwisy;
 
 public interface ISerwisWypozyczen
 {
-    
+    void UtworzWypozyczenie(Osoba osoba, Modele.Sprzet sprzet, DateTime odKiedy, DateTime doKiedy);
+    void ZwrocSprzet(int idWypozyczenia, DateTime dataZwrotu);
+    void AnulujWypozyczenie(int idWypozyczenia);
+    List<Wypozyczenie> PobierzWypozyczeniaOsoby(Osoba osoba);
+    List<Wypozyczenie> PobierzPrzeterminowane(DateTime dataOdniesienia);
+    List<Wypozyczenie> PobierzWszystkie();
+
 }
